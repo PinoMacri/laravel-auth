fdab099b7a619bea5c0c15133c0a274bcba79ff7
PinoMacri <pinonegozio1@outlook.it>
2023-03-07 17:43:01 +0100

    finish projects

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
+Route::get('/', [GuestHomeController::class, "index"]);
+Route::middleware(["auth", "verified"])->name("admin.")->prefix("admin")->group(function(){
+    Route::get("/", [AdminHomeController::class,"index"])->name("home");
+
+    Route::get("/projects", [ProjectController::class,"index"])->name("projects.index");
+    Route::get("/projects/{project}", [ProjectController::class,"show"])->name("projects.show");
+    Route::get("/projects/create", [ProjectController::class,"create"])->name("projects.create");
+    Route::post("/projects", [ProjectController::class,"store"])->name("projects.store");
+    Route::get("/projects{project}/edit", [ProjectController::class,"edit"])->name("projects.edit");
+    Route::put("/projects/{project}", [ProjectController::class,"update"])->name("projects.update");
+    Route::delete("/projects/{projects}", [ProjectController::class,"destroy"])->name("projects.destroy");
 });
 
-Route::get('/admin', function () {
-    return view('admin.home');
-})->middleware(['auth', 'verified'])->name('admin.home');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->name("profile.")->prefix("/profile")->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
 });
 
 require __DIR__.'/auth.php';
